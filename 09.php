d4f499cc73d34416f0702e8977c2d2ddd90623e9
Martin Carreno <martincarreno71@gmail.com>
2025-06-18 04:11:52 -0400

    Conectar DB con prepare
    Explicacion de como conectar a la base de datos con la sintaxis de POO y ademas de aprender sobre statements(stmt) y prepare, para hacer sentencias preparadas como peticiones a la base de datos.
    
    son mas pasos que la otra forma, pero esta asegura la seguridad del proyecto y asdemas el performance

--- 09.php
+++ 09.php
@@ -1,4 +1,32 @@
 <?php include 'includes/header.php';
 
+// Conectar a la BD de MySql
+
+$db = new mysqli('localhost', 'root', 'admin', 'bienesraices_crud');
+
+//forma con stmt y prepare
+
+//crear el query
+$query = "SELECT titulo FROM propiedades";
+
+//preparamos
+$stmt = $db->prepare($query);
+
+//Ejecutamos
+$stmt->execute();
+
+//se crea la variable
+$stmt->bind_result($titulo);//se le asigna el nombre que queramos a la variable
+
+//se asigna el resultado
+$stmt->fetch();
+
+
+//se imprime el resultado
+var_dump($titulo);
+
+// $resultado = $db->query($query);
+
+// var_dump($resultado->fetch_assoc());
 
 include 'includes/footer.php';
