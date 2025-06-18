ad43dff709db7a4f29be11d1c85bdde376834058
Martin Carreno <martincarreno71@gmail.com>
2025-06-18 03:35:04 -0400

    Namespaces Autoload
    Se utiliza composer para utilizar el autoloiad que trae por defecto

--- 08.php
+++ 08.php
@@ -2,25 +2,28 @@
 
 //Incluir las otras clases
 
-require 'clases/Clientes.php';
-require 'clases/Detalles.php';
+// require 'clases/Clientes.php';
 // require 'clases/Detalles.php';
 
-// function mi_autoload($clase){ //Importante, crear este autoload para poder instanciar automatico
+use App\Clientes; //esto nos permite eliminar el namespace cuando se instancia la clase. esta forma se utiliza en laravel
+use App\Detalles;
 
-//     require __DIR__ . '/clases/' . $clase . '.php';
+
+//IMPORTANTE, composer ya trae un autoiload predeterminado, es mejor importarlo al iniciar composer
+
+// function mi_autoload($clase){ //Importante, crear este autoload para poder instanciar automatico
+//     $partes = explode('\\', $clase); //esto nos crea un arreglo con "App\Detalles" separado de esta forma App Detalles.
+
+//     require __DIR__ . '/clases/' . $partes[1] . '.php'; //como es un arreglo, tiene que ser [1] para llamar el nombre de la Clase
 // }
 
 // spl_autoload_register('mi_autoload'); //con esto podemos instanciar las clases automaticamente segun las vaya llamando en el codigo
 
-class Clientes {
-    public  function __construct() {
-        echo "Desde 08.php que contiene los clientes";
-    }
-}
 
-$detalles = new App\Detalles(); // De esta forma se llama la clase con el namespace. Primero va el "namespace" luego "\" y despues el nombre de la clase "Clase()"
-$clientes = new App\Clientes();
-$clientes2 = new Clientes();
+// $detalles = new App\Detalles(); // De esta forma se llama la clase con el namespace. Primero va el "namespace" luego "\" y despues el nombre de la clase "Clase()"
+$detalles = new Detalles(); // con use al principio podemos solo llamar a la Clase
+$clientes = new Clientes();
+
+
 
 include 'includes/footer.php';
