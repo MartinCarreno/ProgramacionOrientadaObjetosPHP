d17c884bc045789ed7bc662cd120bac3440637ec
Martin Carreno <martincarreno71@gmail.com>
2025-06-18 03:11:24 -0400

    Namespaces
    Namespaces para poder isntanciar clases que tengan el mismo nombre,  es la explicacion y la aplicacion de esto.

--- 08.php
+++ 08.php
@@ -2,17 +2,25 @@
 
 //Incluir las otras clases
 
-// require 'clases/Clientes.php';
+require 'clases/Clientes.php';
+require 'clases/Detalles.php';
 // require 'clases/Detalles.php';
 
-function mi_autoload($clase){ //Importante, crear este autoload para poder instanciar automatico
+// function mi_autoload($clase){ //Importante, crear este autoload para poder instanciar automatico
 
-    require __DIR__ . '/clases/' . $clase . '.php';
+//     require __DIR__ . '/clases/' . $clase . '.php';
+// }
+
+// spl_autoload_register('mi_autoload'); //con esto podemos instanciar las clases automaticamente segun las vaya llamando en el codigo
+
+class Clientes {
+    public  function __construct() {
+        echo "Desde 08.php que contiene los clientes";
+    }
 }
 
-spl_autoload_register('mi_autoload'); //con esto podemos instanciar las clases automaticamente segun las vaya llamando en el codigo
-
-$detalles = new Detalles();
-$clientes = new Clientes();
+$detalles = new App\Detalles(); // De esta forma se llama la clase con el namespace. Primero va el "namespace" luego "\" y despues el nombre de la clase "Clase()"
+$clientes = new App\Clientes();
+$clientes2 = new Clientes();
 
 include 'includes/footer.php';
--- clases/Clientes.php
+++ clases/Clientes.php
@@ -1,6 +1,8 @@
 <?php 
 
 
+namespace App; // con este namespace nos permite que al momento de crear clases con el mismo codigo no nos choque el codigo
+
 class Clientes {
     public function __construct(){
         echo "desde la clase Clientes.php";
